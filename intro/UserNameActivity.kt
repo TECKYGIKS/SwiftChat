55e76a5443a089f90d166fa754fd123f1d77e282
APURV CHOUDHARY <104256475+APURVTHECODER@users.noreply.github.com>
2024-08-30 21:18:12 +0530

    Add files via upload

---
+++ intro/UserNameActivity.kt
@@ -0,0 +1,16 @@
+package com.example.randomchat.intro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.randomchat.R
+
+class UserNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_name)
+
+    }
+}
